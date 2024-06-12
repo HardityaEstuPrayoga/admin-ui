9868f4b61b7d5340d7d71e45297ea55748f320bb
unknown <prayogay798@gmail.com>
2024-06-12 17:16:30 +0700

    firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "store-tutorial-1e5c0.firebaseapp.com",
+  projectId: "store-tutorial-1e5c0",
+  storageBucket: "store-tutorial-1e5c0.appspot.com",
+  messagingSenderId: "539478662984",
+  appId: "1:539478662984:web:a5eea9be0ef5aefda39d7d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
